966c911f81a79bf8d549b61d4e139ea10223baac
Parissa <parissa.jamali@gmail.com>
2018-01-25 16:42:45 +0000

    add allClearButton and remove zero from display

--- script.js
+++ script.js
@@ -2,23 +2,39 @@ const display = document.getElementById('display');
 const result = document.getElementById('result');
 const buttons = document.querySelectorAll('.button');
 const equalsButton = document.getElementById('equalsButton');
-const clearButton = document.getElementById('clearButton');
+const allClearButton = document.getElementById('allClearButton');
+const displayClearButton = document.getElementById('displayClearButton');
 
 
-function addToDisplay() {
-    display.innerHTML = (display.innerHTML === "0" ? this.value : display.innerHTML + this.value);
+function addToDisplay() {       //ADD S.TH FOR 0-8   INSTEAD OF -8  //////IS THIS NEEDED?!?!?
+
+    //checks if display and result both have the value of zero remove the 0 in display and add the first digit
+    display.innerHTML = ((((this.value == "*") || (this.value == "/")) && (result.innerHTML === "0") && (display.innerHTML === "0")) ? "error" :
+        ((display.innerHTML === "0") ? this.innerHTML : display.innerHTML + this.innerHTML));
+
+    cached = (cached === "0" ? this.value : cached + this.value);
+
+    //display.innerHTML = (display.innerHTML === "0" ? this.innerHTML : display.innerHTML + this.innerHTML )     ;
 }
 
-function showResult(){
-    result.innerHTML = eval(display.innerHTML);
-    display.innerHTML = eval(display.innerHTML);
+function showResult() {
+    result.innerHTML = eval(cached);
+    display.innerHTML = result.innerHTML;
+    cached = result.innerHTML;
 }
 
 function clearDisplay() {
     display.innerHTML = 0;
+    cached = result.innerHTML;
+};
+
+function allClear() {
     result.innerHTML = 0;
+    display.innerHTML = 0;
+    cached = "";
 };
 
 buttons.forEach(button => button.addEventListener('click', addToDisplay));
 equalsButton.addEventListener('click', showResult);
-clearButton.addEventListener('click', clearDisplay);
+displayClearButton.addEventListener('click', clearDisplay);
+allClearButton.addEventListener('click', allClear);
