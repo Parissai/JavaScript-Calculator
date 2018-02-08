70365cbdd687ad364b2dea55fbcf50bd00056942
Parissa <parissa.jamali@gmail.com>
2018-02-08 16:00:07 +0000

    change cached to array & finish auto*

--- script.js
+++ script.js
@@ -1,69 +1,81 @@
 const display = document.getElementById('display');
-const result = document.getElementById('result');
+const resultDisplay = document.getElementById('result');
 const buttons = document.querySelectorAll('.button');
 const equalsButton = document.getElementById('equalsButton');
 const allClearButton = document.getElementById('allClearButton');
 const displayClearButton = document.getElementById('displayClearButton');
-const arrow = document.getElementById('arrow');
-let cached = "";
+const backspace = document.getElementById('backspace');
+let cached = [];
 
-function addAutoMultiply() {
+/*
+function addAutoMultiply() {    
+}
+*/
+function addToDisplay() {
+    //issue after alert(error) display should return 0 and also cached
+    //checks if display and result both have the value of zero remove the 0 in display and add the first digit
+    display.innerHTML = ((((this.value === "*") || (this.value === "/")) && (resultDisplay.innerHTML === "0") && (display.innerHTML === "0")) ? 
+        alert("error") : ((display.innerHTML === "0") ? 
+            this.innerHTML : display.innerHTML + this.innerHTML));
 
-    //if before log and radical and pi is a digit automatically add *
+        
     const filterInt = function (value) {
         if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) return 'number';
     };
-
-    cached = (filterInt(cached.slice(-1)) == 'number' ? cached + '*' : cached);
-}
-
-function addToDisplay() {       //ADD S.TH FOR 0-8   INSTEAD OF -8  //////IS THIS NEEDED?!?!?
-
-    if (this.classList.contains('math')) addAutoMultiply();
-
-    //checks if display and result both have the value of zero remove the 0 in display and add the first digit
-    display.innerHTML = ((((this.value == "*") || (this.value == "/")) && (result.innerHTML === "0") && (display.innerHTML === "0")) ? "error" :
-        ((display.innerHTML === "0") ? this.innerHTML : display.innerHTML + this.innerHTML));
-
-    cached = (cached === "0" ? this.value : cached + this.value);
-
+    
+    //if before log and radical and pi is a digit automatically add *
+    if (this.classList.contains('math') &&
+        !((cached[cached.length - 1] === '/') || (cached[cached.length - 1] === '+') || (cached[cached.length - 1] === '-') || (cached[cached.length - 1] === '*') || !cached.length)) { 
+            cached.push(`${'*' + this.value}`);}
+    //if after pi is a digit automatically add *
+    else if ((filterInt(this.value) == 'number') && ((cached[cached.length - 1] == 'Math.PI') || (cached[cached.length - 1] == '*Math.PI'))) {
+        cached.push(`${'*' + this.value}`);}
+    else {cached.push(this.value);}
+    
+    console.log(cached);
     //display.innerHTML = (display.innerHTML === "0" ? this.innerHTML : display.innerHTML + this.innerHTML )     ;
 }
 
 function showResult() {
 
-    const openParenthese = cached.split("(").length - 1;
-    const closeParenthese = cached.split(")").length - 1;
+    let cachedStr = cached.join('')
+    const openParenthese = cachedStr.split("(").length - 1;
+    const closeParenthese = cachedStr.split(")").length - 1;
 
     //check if the number of openParenthese and closeParenthese are the same, if not equal them
-    cached = (openParenthese != closeParenthese ? cached + ")".repeat(openParenthese - closeParenthese) : cached);
-
-    result.innerHTML = eval(cached);
-    display.innerHTML = result.innerHTML;
-    cached = result.innerHTML;
+    cachedStr = (openParenthese != closeParenthese ? cachedStr + ")".repeat(openParenthese - closeParenthese) : cachedStr);
+    
+    let result = eval(cachedStr);
+    //cached.length = 0;
+    resultDisplay.innerHTML = result;
+    display.innerHTML = result;    
+    cached = result.toString().split('');
 }
 
 function clearDisplay() {
     display.innerHTML = 0;
-    cached = result.innerHTML;
+    cached = resultDisplay.innerHTML;
 };
 
 function allClear() {
-    result.innerHTML = 0;
+    resultDisplay.innerHTML = 0;
     display.innerHTML = 0;
-    cached = "";
+    cached.length = 0;
 };
 
-function dropLastChar() {//TODO check if the last char is Math.something and drop that all together eg. "Math.PI" cached "Math.P" cached "Math.P09-2"
-    //TODO check if it is the last digit remained is 0 
-    //TODO  % also needs checking   /100   back gives   /10 
-
-    cached = cached.slice(0, -1);
-    display.innerHTML = (display.innerHTML).slice(0, -1);
+function dropLastChar() {           ///issue backspace makes display split  4π√log4  <=   4*Math.PI*Math.sqrt(*Math.log(
+    //check if display is empty or it is 0
+    if (cached.length === 0) { 
+        alert('Nothing to delete');
+    }
+    else {
+        cached = cached.slice(0, -1);
+        display.innerHTML = cached.join('');//(display.innerHTML).slice(0, -1);  //TODO change for log
+    }
 }
 
 buttons.forEach(button => button.addEventListener('click', addToDisplay));
 equalsButton.addEventListener('click', showResult);
 displayClearButton.addEventListener('click', clearDisplay);
 allClearButton.addEventListener('click', allClear);
-arrow.addEventListener('click', dropLastChar);
+backspace.addEventListener('click', dropLastChar);
