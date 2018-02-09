69f5b8e7a7c5a46ba543fd64b3e04e9c79e4969d
Parissa <parissa.jamali@gmail.com>
2018-02-09 13:33:46 +0000

    fix backspace issues

--- script.js
+++ script.js
@@ -14,24 +14,24 @@ function addAutoMultiply() {
 function addToDisplay() {
     //issue after alert(error) display should return 0 and also cached
     //checks if display and result both have the value of zero remove the 0 in display and add the first digit
-    display.innerHTML = ((((this.value === "*") || (this.value === "/")) && (resultDisplay.innerHTML === "0") && (display.innerHTML === "0")) ? 
-        alert("error") : ((display.innerHTML === "0") ? 
+    display.innerHTML = ((((this.value === "*") || (this.value === "/")) && (resultDisplay.innerHTML === "0") && (display.innerHTML === "0")) ?
+        alert("error") : ((display.innerHTML === "0") ?
             this.innerHTML : display.innerHTML + this.innerHTML));
 
-        
+
     const filterInt = function (value) {
         if (/^(\-|\+)?([0-9]+|Infinity)$/.test(value)) return 'number';
     };
-    
+
     //if before log and radical and pi is a digit automatically add *
     if (this.classList.contains('math') &&
-        !((cached[cached.length - 1] === '/') || (cached[cached.length - 1] === '+') || (cached[cached.length - 1] === '-') || (cached[cached.length - 1] === '*') || !cached.length)) { 
+        !((cached[cached.length - 1] === '/') || (cached[cached.length - 1] === '+') || (cached[cached.length - 1] === '-') || (cached[cached.length - 1] === '*') || !cached.length)) {
             cached.push(`${'*' + this.value}`);}
     //if after pi is a digit automatically add *
     else if ((filterInt(this.value) == 'number') && ((cached[cached.length - 1] == 'Math.PI') || (cached[cached.length - 1] == '*Math.PI'))) {
         cached.push(`${'*' + this.value}`);}
     else {cached.push(this.value);}
-    
+
     console.log(cached);
     //display.innerHTML = (display.innerHTML === "0" ? this.innerHTML : display.innerHTML + this.innerHTML )     ;
 }
@@ -44,11 +44,11 @@ function showResult() {
 
     //check if the number of openParenthese and closeParenthese are the same, if not equal them
     cachedStr = (openParenthese != closeParenthese ? cachedStr + ")".repeat(openParenthese - closeParenthese) : cachedStr);
-    
+
     let result = eval(cachedStr);
     //cached.length = 0;
     resultDisplay.innerHTML = result;
-    display.innerHTML = result;    
+    display.innerHTML = result;
     cached = result.toString().split('');
 }
 
@@ -63,14 +63,22 @@ function allClear() {
     cached.length = 0;
 };
 
-function dropLastChar() {           ///issue backspace makes display split  4π√log4  <=   4*Math.PI*Math.sqrt(*Math.log(
+function dropLastChar() {           
+    //store display.innerHTML to avoid splitting 4π√log4  <=   4*Math.PI*Math.sqrt(*Math.log(
+    const newCached = display.innerHTML;
+
     //check if display is empty or it is 0
     if (cached.length === 0) { 
         alert('Nothing to delete');
     }
+    //check if last char is Math.log and delete 'log' from display
+    else if (newCached.slice(-3) === 'log'){ 
+        cached = cached.slice(0, -1);        
+        display.innerHTML = newCached.slice(0, -3);
+    }
     else {
         cached = cached.slice(0, -1);
-        display.innerHTML = cached.join('');//(display.innerHTML).slice(0, -1);  //TODO change for log
+        display.innerHTML = newCached.slice(0, -1);
     }
 }
 
